c61aedc69b6b61772bb56d058eb0ea4377b4cc64
toyama satoshi <toyamarinyon@gmail.com>
2016-09-15 20:16:07 +0900

    User auth

--- fuel/app/views/user/create.php
+++
@@ -1,7 +0,0 @@
-<h2>New <span class='muted'>User</span></h2>
-<br>
-
-<?php echo render('user/_form'); ?>
-
-
-<p><?php echo Html::anchor('user', 'Back'); ?></p>
--- fuel/app/views/user/edit.php
+++
@@ -1,7 +0,0 @@
-<h2>Editing <span class='muted'>User</span></h2>
-<br>
-
-<?php echo render('user/_form'); ?>
-<p>
-	<?php echo Html::anchor('user/view/'.$user->id, 'View'); ?> |
-	<?php echo Html::anchor('user', 'Back'); ?></p>
