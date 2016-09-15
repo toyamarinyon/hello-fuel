d3fb962c692ce407c1250f984b426afed5cbfa8c
toyama satoshi <toyamarinyon@gmail.com>
2016-09-15 11:35:53 +0900

    Add user

---
+++ fuel/app/classes/controller/user.php
@@ -0,0 +1,132 @@
+<?php
+class Controller_User extends Controller_Template
+{
+
+	public function action_index()
+	{
+		$data['users'] = Model_User::find('all');
+		$this->template->title = "Users";
+		$this->template->content = View::forge('user/index', $data);
+
+	}
+
+	public function action_view($id = null)
+	{
+		is_null($id) and Response::redirect('user');
+
+		if ( ! $data['user'] = Model_User::find($id))
+		{
+			Session::set_flash('error', 'Could not find user #'.$id);
+			Response::redirect('user');
+		}
+
+		$this->template->title = "User";
+		$this->template->content = View::forge('user/view', $data);
+
+	}
+
+	public function action_create()
+	{
+		if (Input::method() == 'POST')
+		{
+			$val = Model_User::validate('create');
+
+			if ($val->run())
+			{
+				$user = Model_User::forge(array(
+					'name' => Input::post('name'),
+					'email' => Input::post('email'),
+				));
+
+				if ($user and $user->save())
+				{
+					Session::set_flash('success', 'Added user #'.$user->id.'.');
+
+					Response::redirect('user');
+				}
+
+				else
+				{
+					Session::set_flash('error', 'Could not save user.');
+				}
+			}
+			else
+			{
+				Session::set_flash('error', $val->error());
+			}
+		}
+
+		$this->template->title = "Users";
+		$this->template->content = View::forge('user/create');
+
+	}
+
+	public function action_edit($id = null)
+	{
+		is_null($id) and Response::redirect('user');
+
+		if ( ! $user = Model_User::find($id))
+		{
+			Session::set_flash('error', 'Could not find user #'.$id);
+			Response::redirect('user');
+		}
+
+		$val = Model_User::validate('edit');
+
+		if ($val->run())
+		{
+			$user->name = Input::post('name');
+			$user->email = Input::post('email');
+
+			if ($user->save())
+			{
+				Session::set_flash('success', 'Updated user #' . $id);
+
+				Response::redirect('user');
+			}
+
+			else
+			{
+				Session::set_flash('error', 'Could not update user #' . $id);
+			}
+		}
+
+		else
+		{
+			if (Input::method() == 'POST')
+			{
+				$user->name = $val->validated('name');
+				$user->email = $val->validated('email');
+
+				Session::set_flash('error', $val->error());
+			}
+
+			$this->template->set_global('user', $user, false);
+		}
+
+		$this->template->title = "Users";
+		$this->template->content = View::forge('user/edit');
+
+	}
+
+	public function action_delete($id = null)
+	{
+		is_null($id) and Response::redirect('user');
+
+		if ($user = Model_User::find($id))
+		{
+			$user->delete();
+
+			Session::set_flash('success', 'Deleted user #'.$id);
+		}
+
+		else
+		{
+			Session::set_flash('error', 'Could not delete user #'.$id);
+		}
+
+		Response::redirect('user');
+
+	}
+
+}
--- fuel/app/config/development/db.php
+++ fuel/app/config/development/db.php
@@ -6,7 +6,7 @@
 return array(
 	'default' => array(
 		'connection'  => array(
-			'dsn'        => 'mysql:host=localhost;dbname=fuel_dev',
+			'dsn'        => 'mysql:host=localhost;dbname=fuel_dev;unix_socket=/var/lib/mysql/mysql.sock',
 			'username'   => 'root',
 			'password'   => 'root',
 		),
